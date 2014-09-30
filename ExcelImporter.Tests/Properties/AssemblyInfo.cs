b575f1e0e6c20183585332c56097846bc2374a49
jlumijarvi <janne.kr.lumijarvi@gmail.com>
2014-10-01 02:37:32 +0300

    namings

--- ExcelImporter.Tests/Properties/AssemblyInfo.cs
+++ ExcelImporter.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExcelImporter2.Tests")]
+[assembly: AssemblyTitle("ExcelImporter.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExcelImporter2.Tests")]
+[assembly: AssemblyProduct("ExcelImporter.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ExcelImporter2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExcelImporter.Global" Language="C#" %>
